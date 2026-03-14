f7565a90f12d7d6bc24be73ebaedc4f298cdfe33
Michał Kostkowski <Nestea@MacBook-Air-Ewa.local>
2026-03-14 13:04:50 +0100

    a

--- myrpg/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.title = "Wasteland Chronicles"
-    t.version = "11.4"
-    t.window.width = 1024
-    t.window.height = 768
-    t.window.resizable = false
-    t.console = false
-end
