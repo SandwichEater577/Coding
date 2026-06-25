67fc03c6041ac994ccb62218ca1c30970d994b5e
NesTEa <michalkostkowski062@gmail.com>
2026-06-25 14:00:49 +0200

    portfolio in progress

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import XMBMenu from "./components/XMBMenu";
+
+function App() {
+  return (
+    <>
+      <XMBMenu />
+    </>
+  );
+}
+
+export default App;
---
+++ Portfolio/src/components/XMBMenu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./XMBMenu.css";
+
+/**
+ * XMB (XrossMediaBar) Menu Scaffold
+ * Build your PS3-like menu logic here!
+ */
+export default function XMBMenu() {
+  return <div className="xmb-container"></div>;
+}
